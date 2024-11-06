8c3eb77544abf9e908e39e2f9cf3246b32f632fd
Ana Mel <anamelbossolansilva@gmail.com>
2024-11-06 18:55:26 -0300

    segundo commit

--- index.js
+++ index.js
@@ -7,13 +7,27 @@ class heroi {
 
         atacar() {
         let ataque
-        if (tipo = "mago") {
-            console.log("")
+        if(this.tipo == "mago"){
+            ataque = "magia"
+        }else if(this.tipo == "guerreiro"){
+            ataque = "espada"
+        }else if(this.tipo == "monge"){
+            ataque = "artes marciais"
+        }else if(this.tipo == "ninja"){
+            ataque = "shuriken"
+        }else{
+            ataque = "ataque mistério"
         }
+
+        console.log(`o ${this.tipo} atacou usando ${ataque}`)
+    
     }
 }
 
 
 let heroiEu = new heroi("Namel", 20, "ninja")
+let heroiParceiro = new heroi("Juni", 19, "guerreiro")
 
-console.log(heroiEu)
+
+heroiEu.atacar()
+heroiParceiro.atacar()
