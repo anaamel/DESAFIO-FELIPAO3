30ac26ba50ee28b783b403ce73628500424e7abf
Ana Mel <anamelbossolansilva@gmail.com>
2024-11-06 18:37:36 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,19 @@
+class heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+
+        atacar() {
+        let ataque
+        if (tipo = "mago") {
+            console.log("")
+        }
+    }
+}
+
+
+let heroiEu = new heroi("Namel", 20, "ninja")
+
+console.log(heroiEu)
